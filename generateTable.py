1505b3de54f239b0c89bdcc73b98fde8b1a941dd
jesusjzp <jesusjzp@gmail.com>
2013-10-06 22:23:32 +0200

    construct input format for Galicia

--- generateTable.py
+++ generateTable.py
@@ -20,6 +20,8 @@ else:
 	inFile3 = sys.argv[3]
 	inFile4 = sys.argv[4]
 
+###########################################################
+
 im = Image.open(inFile1)
 
 multilist = [[0 for col in range(4)] for row in range(im.size[0]*im.size[1])]
@@ -54,4 +56,34 @@ for i in range(im.size[0]):
 			multilist[i*im.size[1]+j][3] = 1
 		# print i, j, i*im.size[1]+j, im.getpixel((i,j))
 
-print multilist
+# print len(multilist)
+
+###########################################################
+
+f = file('testBin.slf','w')
+
+f.write('[Lattice]'+'\n')
+f.write(str(len(multilist))+'\n')
+f.write('4'+'\n')
+
+f.write('[Objects]'+'\n')
+for i in range(im.size[0]):
+	for j in range(im.size[1]):
+		f.write('('+str(i)+','+str(j)+')'+'\n')
+
+
+f.write('[Attributes]'+'\n')
+f.write('p'+'\n')
+f.write('w'+'\n')
+f.write('n'+'\n')
+f.write('s'+'\n')
+
+f.write('[relation]'+'\n')
+for i in range(0,len(multilist)):
+	for j in range(0,len(multilist[i])):
+		f.write(str(multilist[i][j]) + ' ')
+	f.write('\n')
+
+f.close()
+
+
