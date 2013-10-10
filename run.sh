16f487d4c0f0cd56b6eab128947681c3dbb339ef
jesusjzp <jesusjzp@gmail.com>
2013-10-10 23:05:59 +0200

    edit run.sh

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# generate ppm with simple algorithm
+# Generate ppm with simple binarization algorithm
 python binarizezhipeng.py test.jpg test_p.ppm
 
-# generate ppm with simple algorithm
+# Generate ppm with algorithm of Niblack (1986)
 ./binarizewolfjolion n test.jpg test_n.ppm
 
-# generate ppm with simple algorithm
+# Generate ppm with algorithm of Wolf et al. (2001)
 ./binarizewolfjolion w test.jpg test_w.ppm
 
-# generate ppm with simple algorithm
+# Generate ppm with algorithm of Sauvola et al. (1997)
 ./binarizewolfjolion s test.jpg test_s.ppm
 
 # construct format input for Galicia, etc.
